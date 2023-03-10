7cfa5bdc1c3b497a15fa32a32e00bdd35186e38d
ynb925 <ynb4mail@gmail.com>
2023-03-10 16:41:53 +0200

    init

---
+++ out/production/max_EDU/test/com/src/stream_api/StreamApi1.java
@@ -0,0 +1,23 @@
+package stream_api;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Створити List<String> за допомогою методу Stream.of і наповнити його днями неділі, Sunday, Monday...
+ * Потім в цьому ж стрімі відфільтрувати його по довжині слова щоб розмір назви дня був не довший ніж 7 символів включно,
+ * далі додати до кожної назви дня слово "First " з пробілом, далі зібрати все у список за допомогою методу collect.
+ * У методі main використати метод forEach для створеного списку
+ * І вивести у консоль каожний напис.
+ */
+public class StreamApi1 {
+    public static void main(String[] args) {
+        List<String> weekDays = Stream.of("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+                .filter(x -> x.length() <= 7)
+                .map(day -> "First " + day)
+                .collect(Collectors.toList());
+
+        weekDays.forEach(System.out::println);
+    }
+}
---
+++ out/production/max_EDU/test/com/src/stream_api/StreamApi2.java
@@ -0,0 +1,49 @@
+package stream_api;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+/**
+ * Створити List<String> numbers за допомогою методу List.of додати в нього назви цифр словами (one, two, three..) від 0 до 9.
+ * В наступній стрічці зробити стрім, конвертувати в числа за допомогою приватного методу convertToNumber який буде приймати слово і повертати цифру,
+ * можна використати switch, бажано нову версію Java 17.
+ * Потім в тому ж стрімі викликати метод collect та в ньому метод summingInt щоб порахувати суму чисел.
+ */
+public class StreamApi2 {
+
+    public static void main(String[] args) {
+        Integer sum = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine").stream()
+                .collect(Collectors.summingInt(StreamApi2::convertToNumber));
+
+        System.out.println(sum);
+    }
+
+    private static Integer convertToNumber(String s) {
+
+        switch (s) {
+            case "zero":
+                return 0;
+            case "one":
+                return 1;
+            case "two":
+                return 2;
+            case "three":
+                return 3;
+            case "four":
+                return 4;
+            case "five":
+                return 5;
+            case "six":
+                return 6;
+            case "seven":
+                return 7;
+            case "eight":
+                return 8;
+            case "nine":
+                return 9;
+            default:
+                throw new RuntimeException();
+        }
+    }
+}
