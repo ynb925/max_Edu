e5cbece933869ff6406f536fc0bfb03aa3fca778
ynb925 <ynb4mail@gmail.com>
2023-03-11 23:27:01 +0200

    last fix

--- src/com/serial/Body.java
+++ src/com/serial/Body.java
@@ -2,6 +2,17 @@ package com.serial;
 
 import java.io.Serializable;
 
+/**
+ * Створити клас Body який імплементує інтерфейс Serializable зі змінними int height
+ * та String name. У класі Test створити метод main і зробити в ньому блок try-catch
+ * де буде спочатку реалізована серіалізація задопомогою класу FileOutputStream і
+ * ObjectOutputStream і його методом writeObject; А після того в наступному блоці
+ * try-catch реалізувати десеріалізацію задопомогою класів FileInputStream та
+ * ObjectInputStream та його методу readObject. Не забувати закривати стріми.
+ * Вивести у консоль те що серіалізовувалось і десеріалізовувалось. Виключення обробити
+ * виведенням на екран типу ексепшену.
+ */
+
 public class Body implements Serializable {
 
     private static final long serialVersionUID = 14376375L;
--- src/com/serial/Test.java
+++ src/com/serial/Test.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.file.Path;
 
 public class Test {
+
     public static void main(String[] args) {
         Path path = Path.of("res", "serialObj.txt");
 
@@ -12,17 +13,17 @@ public class Test {
     }
 
     private static void serializeWriteObj(Path path) {
-        try (ObjectOutputStream outFile = new ObjectOutputStream(new FileOutputStream(path.toFile()))) {
+        try (ObjectOutputStream fileOut = new ObjectOutputStream(new FileOutputStream(path.toFile()))) {
             Body body1 = new Body(34, "First");
-            outFile.writeObject(body1);
+            fileOut.writeObject(body1);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     private static void deSerializeReadObj(Path path) {
-        try (ObjectInputStream inFile = new ObjectInputStream(new FileInputStream(path.toFile()))) {
-            Object obj = inFile.readObject();
+        try (ObjectInputStream fileIn = new ObjectInputStream(new FileInputStream(path.toFile()))) {
+            Object obj = fileIn.readObject();
             System.out.println(obj);
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
